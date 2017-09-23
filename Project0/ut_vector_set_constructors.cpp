575af4f2cdc753683bdf7c9d6e9cf63711bf03f4
Cody Grant <codygrant0324@gmail.com>
2017-09-23 10:42:48 -0400

    added move constructor test

--- Project0/ut_vector_set_constructors.cpp
+++ Project0/ut_vector_set_constructors.cpp
@@ -29,3 +29,15 @@ BOOST_AUTO_TEST_CASE(copy_constructor) {
 	BOOST_CHECK_EQUAL(*vs1.begin(), *vs2.begin());	// check that the values are the same
 	BOOST_CHECK(!vs2.empty());
 }
+
+BOOST_AUTO_TEST_CASE(move_constructor) {
+
+	// fill one
+	vector_set<size_t> vs1 = { 1,2,3,4,5 };
+	size_t original_size = vs1.size();
+	// move filled one into new one
+	vector_set<size_t> vs2(move(vs1));
+	BOOST_CHECK(vs1.empty());
+	BOOST_CHECK(!vs2.empty());
+	BOOST_CHECK_EQUAL(vs2.size(), original_size);
+}
