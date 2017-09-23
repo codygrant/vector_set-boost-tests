3bd49db1a7a4ba8b5d00aac7406d3bb5a696cec2
Cody Grant <codygrant0324@gmail.com>
2017-09-23 09:01:31 -0400

    added constructor boost tests

--- Project0/ut_vector_set_constructors.cpp
+++ Project0/ut_vector_set_constructors.cpp
@@ -1,5 +1,31 @@
 #include <boost/test/unit_test.hpp>
+#include <boost/mpl/list.hpp>
 
 #include "vector_set.hpp"
 #include <iostream>
 using namespace std;
+
+using constructor_types = boost::mpl::list<int, long, unsigned char, double, std::string>;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(destructor, T, constructor_types) {
+
+	vector_set<T> vs;
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(default_constructor, T, constructor_types) {
+
+	vector_set<T> vs;
+	BOOST_CHECK(vs.size() == 0);
+	BOOST_CHECK(vs.empty());
+}
+
+BOOST_AUTO_TEST_CASE(copy_constructor) {
+
+	// fill one
+	vector_set<size_t> vs1 = { 1,2,3,4,5 };
+	// copy filled one into new one
+	vector_set<size_t> vs2(vs1);
+	BOOST_CHECK_EQUAL(vs1.size(), vs2.size());
+	BOOST_CHECK_EQUAL(*vs1.begin(), *vs2.begin());	// check that the values are the same
+	BOOST_CHECK(!vs2.empty());
+}
