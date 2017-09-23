998bcb1438b55882affc35f937ad10143571c2dc
Cody Grant <codygrant0324@gmail.com>
2017-09-23 17:08:13 -0400

    added empty() test

--- Project0/ut_vector_set_capacity.cpp
+++ Project0/ut_vector_set_capacity.cpp
@@ -3,3 +3,12 @@
 #include "vector_set.hpp"
 #include <iostream>
 using namespace std;
+
+BOOST_AUTO_TEST_CASE(capacity_tests) {
+
+	// 1. Empty
+	vector_set<size_t> vs1;
+	BOOST_CHECK(vs1.empty());
+	vector_set<unsigned char> vs2{ 'a','z','t' };
+	BOOST_CHECK(!vs2.empty());
+}
