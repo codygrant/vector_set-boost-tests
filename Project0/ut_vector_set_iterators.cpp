65e2721b4b388bd5e1ff14ed3199650ca39a6480
Cody Grant <codygrant0324@gmail.com>
2017-09-25 09:12:06 -0400

    iterator tests setup

--- Project0/ut_vector_set_iterators.cpp
+++ Project0/ut_vector_set_iterators.cpp
@@ -3,3 +3,36 @@
 #include "vector_set.hpp"
 #include <iostream>
 using namespace std;
+
+BOOST_AUTO_TEST_CASE(iterator_tests) {
+
+	// 1. begin() test
+
+	// 2. end() test
+}
+
+BOOST_AUTO_TEST_CASE(const_iterator_tests) {
+
+	// 1. cbegin() test
+
+	// 2. cend() test
+
+	// 3. const begin() test
+
+	// 4. const end() test
+}
+
+BOOST_AUTO_TEST_CASE(reverse_iterator_tests) {
+
+	// 1. rbegin() test
+
+	// 2. rend() test
+
+	// 3. crbegin() test
+
+	// 4. crend() test
+
+	// 5. const rbegin() test
+
+	// 6. const rend() test
+}
