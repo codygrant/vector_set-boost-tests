c83453bf2c5d1d935f656d8eef07836b70a5d2d2
Cody Grant <codygrant0324@gmail.com>
2017-09-27 16:56:22 -0400

    added initial lookup tests

--- Project0/ut_vector_set_lookup.cpp
+++ Project0/ut_vector_set_lookup.cpp
@@ -9,3 +9,26 @@ Purpose:		Perform Boost Tests on all lookup functions of the vector_set class
 #include "vector_set.hpp"
 #include <iostream>
 using namespace std;
+
+// 1. find()
+BOOST_AUTO_TEST_CASE(lookup_find_tests) {
+
+	vector_set<char> vs1{ 'y','r','u','a','c','m' };
+	auto letter = vs1.find('c');
+	BOOST_CHECK_EQUAL(*letter, 'c');					// dereference to get value
+
+
+}
+
+// 2. lower_bound()
+BOOST_AUTO_TEST_CASE(lookup_lower_bound_tests) {
+
+	// lower_bound returns the iterator pointing to first element in the
+	// specified range
+	vector_set<int> vs1{ 100,200,300,300,300,600,800,800 };
+	auto num = vs1.lower_bound(300);
+	BOOST_CHECK_EQUAL(*num, 300);
+	// previous element should be '200'
+	--num;
+	BOOST_CHECK_MESSAGE(*num == 200, "Iterator should be pointing to '200' not " << *num);
+}
