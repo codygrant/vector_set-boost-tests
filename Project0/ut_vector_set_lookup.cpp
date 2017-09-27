b6f3e26caa1daac036bbbf6f841b1d7ead327df7
Cody Grant <codygrant0324@gmail.com>
2017-09-27 17:36:13 -0400

    small changes to lookup tests

--- Project0/ut_vector_set_lookup.cpp
+++ Project0/ut_vector_set_lookup.cpp
@@ -23,12 +23,12 @@ BOOST_AUTO_TEST_CASE(lookup_find_tests) {
 // 2. lower_bound()
 BOOST_AUTO_TEST_CASE(lookup_lower_bound_tests) {
 
-	// lower_bound returns the iterator pointing to first element in the
+	// lower_bound returns an iterator pointing to first element in the
 	// specified range
 	vector_set<int> vs1{ 100,200,300,300,300,600,800,800 };
 	auto num = vs1.lower_bound(300);
 	BOOST_CHECK_EQUAL(*num, 300);
 	// previous element should be '200'
 	--num;
-	BOOST_CHECK_MESSAGE(*num == 200, "Iterator should be pointing to '200' not " << *num);
+	BOOST_CHECK_MESSAGE(*num == 200, "lower_bound() isn't working correctly! Should be pointing to 200 not " << *num);
 }
