500c623f5ccaa9bd1c4b1fa8685a4def9f7434b3
Cody Grant <codygrant0324@gmail.com>
2017-09-27 16:19:03 -0400

    added capacity() tests

--- Project0/ut_vector_set_capacity.cpp
+++ Project0/ut_vector_set_capacity.cpp
@@ -10,18 +10,51 @@ Purpose:		Perform Boost Tests on all capacity functions of the vector_set class
 #include <iostream>
 using namespace std;
 
-BOOST_AUTO_TEST_CASE(capacity_tests) {
+// 1. Empty()
+BOOST_AUTO_TEST_CASE(capacity_empty_tests) {
 
-	// 1. Empty
 	vector_set<size_t> vs1;
 	BOOST_CHECK(vs1.empty());
 	vector_set<unsigned char> vs2{ 'a','z','t' };
 	BOOST_CHECK(!vs2.empty());
+	vector_set<string> vs3{ "cody","grant","london","fanshawe" };
+	BOOST_CHECK_EQUAL(vs3.empty(), false);
+}
 
-	// 2. Size
+// 2. Size()
+BOOST_AUTO_TEST_CASE(capacity_size_tests) {
+
+	vector_set<size_t> vs1;
 	BOOST_CHECK_EQUAL(vs1.size(), 0);
+	vector_set<unsigned char> vs2{ 'a','z','t' };
 	BOOST_CHECK_EQUAL(vs2.size(), 3);
+	vector_set<string> vs3{ "cody","grant","london","fanshawe" };
+	unsigned vs3_size = vs3.size();			// store in a variable and check
+	BOOST_CHECK(vs3_size == 4);
+}
 
-	// 3. Capacity
-	BOOST_CHECK_EQUAL(vs1.capacity(), 0);	// an un-initialized STL container should have cap = 0	
+// 3. Capacity
+BOOST_AUTO_TEST_CASE(capacity_tests) {
+
+	// check capacity of various sized containers
+	vector_set<size_t> vs1;
+	BOOST_CHECK_EQUAL(vs1.capacity(), 0);	// an un-initialized STL container should have cap = 0
+	vector_set<unsigned char> vs2{ 'a','z','t' };
+	BOOST_CHECK(vs2.capacity() >= 3);
+	vector_set<double> vs3{ 67.8,47.9,106.9,6,95.9,25.7,10.8,174.7,11.6,7.8 };
+	unsigned vs3_size = vs3.size();
+	BOOST_CHECK_EQUAL(vs3_size, 10);
+}
+
+// 4. Max_size()
+BOOST_AUTO_TEST_CASE(capacity_max_size_tests) {
+
+	//vector_set<char> vs2;
+	//cout << (double)numeric_limits<char>::max() << endl;
+	//cout << sizeof(char) << endl;
+	//cout << vs2.max_size() << endl;
+	//vector_set<short> vs3;
+	//cout << vs3.max_size() << endl;
+	//vector_set<long long> vs4;
+	//cout << vs4.max_size() << endl;
 }
