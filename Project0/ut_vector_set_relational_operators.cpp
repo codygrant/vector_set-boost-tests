d938d88daeaa670e6d3a6c1c5ff5d7056e0a37b8
Cody Grant <codygrant0324@gmail.com>
2017-09-27 17:43:45 -0400

    setup all boost tests for relational operators

--- Project0/ut_vector_set_relational_operators.cpp
+++ Project0/ut_vector_set_relational_operators.cpp
@@ -9,3 +9,33 @@ Purpose:		Perform Boost Tests on all relational operators of
 #include "vector_set.hpp"
 #include <iostream>
 using namespace std;
+
+// 1. '==' operator
+BOOST_AUTO_TEST_CASE(relational_operator_equality_tests) {
+
+
+}
+
+// 2. '<' operator
+BOOST_AUTO_TEST_CASE(relational_operator_less_than_tests) {
+
+
+}
+
+// 3. '>' operator
+BOOST_AUTO_TEST_CASE(relational_operator_greater_than_tests) {
+
+
+}
+
+// 4. '>=' operator
+BOOST_AUTO_TEST_CASE(relational_operator_greater_than_equal_tests) {
+
+
+}
+
+// 5. '<=' operator
+BOOST_AUTO_TEST_CASE(relational_operator_less_than_equal_tests) {
+
+
+}
