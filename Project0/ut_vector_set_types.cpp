2cb83e87e5c654fa10132fdb139e5c7a9e837198
Cody Grant <codygrant0324@gmail.com>
2017-09-29 18:20:35 -0400

    completed all types boost checks

--- Project0/ut_vector_set_types.cpp
+++ Project0/ut_vector_set_types.cpp
@@ -16,8 +16,18 @@ using namespace std;
 using test_types = boost::mpl::list<int, long, double, char, std::string>;
 BOOST_AUTO_TEST_CASE_TEMPLATE(type_test, T, test_types) {
 
-	//BOOST_CHECK(typeid(vector_set<T>::value_type) == typeid(std::string));
-	//BOOST_CHECK(typeid(vector_set<T>::reference) == typeid(std::string&));
-	//BOOST_CHECK(typeid(vector_set<T>::const_reference) == typeid(std::string const&));
-	//BOOST_CHECK(typeid(vector_set<T>::size_type) == typeid(std::size_t));
+	BOOST_CHECK(typeid(vector_set<T>::key_type) == typeid(T));
+	BOOST_CHECK(typeid(vector_set<T>::value_type) == typeid(T));
+	BOOST_CHECK(typeid(vector_set<T>::size_type) == typeid(std::size_t));
+	BOOST_CHECK(typeid(vector_set<T>::difference_type) == typeid(std::ptrdiff_t));
+	BOOST_CHECK(typeid(vector_set<T>::key_compare) == typeid(std::less<T>));
+	BOOST_CHECK(typeid(vector_set<T>::value_compare) == typeid(std::less<T>));
+	BOOST_CHECK(typeid(vector_set<T>::reference) == typeid(vector_set<T>::value_type&));
+	BOOST_CHECK(typeid(vector_set<T>::const_reference) == typeid(const vector_set<T>::value_type&));
+	BOOST_CHECK(typeid(vector_set<T>::pointer) == typeid(vector_set<T>::value_type*));
+	BOOST_CHECK(typeid(vector_set<T>::const_pointer) == typeid(vector_set<T>::value_type const*));
+	BOOST_CHECK(typeid(vector_set<T>::iterator) == typeid(vector_set<T>::const_pointer));
+	BOOST_CHECK(typeid(vector_set<T>::const_iterator) == typeid(vector_set<T>::const_pointer));
+	BOOST_CHECK(typeid(vector_set<T>::reverse_iterator) == typeid(std::reverse_iterator<vector_set<T>::iterator>));
+	BOOST_CHECK(typeid(vector_set<T>::const_reverse_iterator) == typeid(std::reverse_iterator<vector_set<T>::const_iterator>));
 }
