2b06ba2e6b544b2ba01c17380b5d57be49f50cd7
Cody Grant <codygrant0324@gmail.com>
2017-09-29 17:00:44 -0400

    started types checks

--- Project0/ut_vector_set_types.cpp
+++ Project0/ut_vector_set_types.cpp
@@ -6,7 +6,18 @@ Purpose:		Perform Boost Tests on type definitions of
 */
 
 #include <boost/test/unit_test.hpp>
+#include <boost/mpl/list.hpp>
 
 #include "vector_set.hpp"
 #include <iostream>
 using namespace std;
+
+#include <typeinfo>
+using test_types = boost::mpl::list<int, long, double, char, std::string>;
+BOOST_AUTO_TEST_CASE_TEMPLATE(type_test, T, test_types) {
+
+	//BOOST_CHECK(typeid(vector_set<T>::value_type) == typeid(std::string));
+	//BOOST_CHECK(typeid(vector_set<T>::reference) == typeid(std::string&));
+	//BOOST_CHECK(typeid(vector_set<T>::const_reference) == typeid(std::string const&));
+	//BOOST_CHECK(typeid(vector_set<T>::size_type) == typeid(std::size_t));
+}
