d62fb066e86851a62a79fab364aec5162a7c4d6c
Cody Grant <codygrant0324@gmail.com>
2017-09-28 21:47:38 -0400

    added single erase() test

--- Project0/ut_vector_set_mod_erase.cpp
+++ Project0/ut_vector_set_mod_erase.cpp
@@ -10,3 +10,13 @@ Purpose:		Perform Boost Tests on the erase() modifier function of
 #include "vector_set.hpp"
 #include <iostream>
 using namespace std;
+
+// 1. Erase a single element
+BOOST_AUTO_TEST_CASE(erase_single_test) {
+
+	vector_set<int> vs1{ 5,9,23,43,76,563 };
+	// create an iterator for erase()
+	auto num = vs1.begin();
+	vs1.erase(num);
+	BOOST_CHECK(*num == 9);					// '5' should be erased and the elements shifted to the left 1 element
+}
