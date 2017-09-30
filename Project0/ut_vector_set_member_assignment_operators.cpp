e57e822e8de6cbaf7460e73a770d1feae10a2302
Cody Grant <codygrant0324@gmail.com>
2017-09-29 22:45:07 -0400

    added initializer assignment checks

--- Project0/ut_vector_set_member_assignment_operators.cpp
+++ Project0/ut_vector_set_member_assignment_operators.cpp
@@ -6,31 +6,34 @@ Purpose:		Perform Boost Tests on all member assignment operators
 */
 
 #include <boost/test/unit_test.hpp>
+#include <boost/mpl/list.hpp>
 
 #include "vector_set.hpp"
 #include <iostream>
 using namespace std;
 
-BOOST_AUTO_TEST_CASE(copy_assignment_operator) {
+using num_char_types = boost::mpl::list<short, int, long, long long, double, char>;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(copy_assignment_operator, T, num_char_types) {
 
 	// fill one
-	vector_set<size_t> vs1{ 1,2,3,4,5 };
-	vector_set<size_t> vs2;
+	vector_set<T> vs1{ 1,2,3,4,5 };
+	vector_set<T> vs2;
 	vs2 = vs1;
 	BOOST_CHECK_EQUAL(vs1.size(), vs2.size());
 	BOOST_CHECK_EQUAL(*vs1.begin(), *vs2.begin());	// check that the values are the same
 	BOOST_CHECK(!vs2.empty());
 
 	// copy empty one into full one
-	vector_set<size_t> vs3;
+	vector_set<T> vs3;
 	vs2 = vs3;
 	BOOST_CHECK(vs2.empty());
 	BOOST_CHECK_EQUAL(vs2.size(), 0);
 	BOOST_CHECK(vs3.empty());
 
 	// copy different sized one
-	vector_set<size_t> vs4{ 100,200,300,400,500,600,700 };
-	vector_set<size_t> vs5{ 1,2,3,4,5 };
+	vector_set<T> vs4{ 1,2,3,4,5,6,7 };
+	vector_set<T> vs5{ 1,2,3,4,5 };
 	size_t vs4_size = vs4.size();
 	size_t vs5_original_size = vs5.size();
 	vs5 = vs4;
@@ -38,11 +41,11 @@ BOOST_AUTO_TEST_CASE(copy_assignment_operator) {
 	BOOST_CHECK(vs5.size() != vs5_original_size);
 }
 
-BOOST_AUTO_TEST_CASE(move_assignment_operator) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(move_assignment_operator, T, num_char_types) {
 
 	// fill one
-	vector_set<size_t> vs1{ 1,2,3,4,5 };
-	vector_set<size_t> vs2;
+	vector_set<T> vs1{ 1,2,3,4,5 };
+	vector_set<T> vs2;
 	vs2 = move(vs1);
 	BOOST_CHECK_EQUAL(vs1.size(), 0);
 	BOOST_CHECK(vs1.empty());
@@ -50,9 +53,17 @@ BOOST_AUTO_TEST_CASE(move_assignment_operator) {
 	BOOST_CHECK(!vs2.empty());
 
 	// move empty one into full one
-	vector_set<size_t> vs3;
+	vector_set<T> vs3;
 	vs2 = move(vs3);
 	BOOST_CHECK(vs2.empty() && vs3.empty());
 	BOOST_CHECK_EQUAL(vs2.size(), 0);
 	BOOST_CHECK_EQUAL(vs3.size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(initializer_assignment_operator, T, num_char_types) {
+
+	// fill one
+	vector_set<T> vs1 = { 1,2,3,4,5 };
+	BOOST_CHECK_EQUAL(vs1.size(), 5);
+	BOOST_CHECK(!vs1.empty());
 }
